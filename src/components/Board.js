31975aced7c76f17e771dcd0ca8223ebc984b55a
Paula Herrera <herrera.paula5@gmail.com>
2021-03-02 14:54:25 -0700

    Added custom board and styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -14,6 +14,7 @@ const Board = () => {
   const [mineLocations, setMineLocations] = useState([]);
   const [gameOver, setGameOver] = useState(false);
   const [won, setWon] = useState(false);
+  const [customRows, setCustomRows] = useState(0);
 
   useEffect(() => {
     freshBoard(rows, cols, bombs);
@@ -47,7 +48,7 @@ const Board = () => {
   }
 
   // Reset Game
-  const reset = (r, c, b) => {
+  const reset = (r = rows, c = cols, b = bombs) => {
     freshBoard(rows, cols, bombs);
     setGameOver(false);
     setWon(false);
@@ -78,75 +79,110 @@ const Board = () => {
 
   return (
     <>
-    <div>
-      <div className="header">
-        <div className="shiba">
-          <img src={Shiba} alt="Shiba"></img>
+      <div>
+        <div className="header">
+          <div className="shiba">
+            <img src={Shiba} alt="Shiba"></img>
+          </div>
+          <div>
+            <h1>Dogesweeper</h1>
+          </div>
         </div>
-        <div>
-          <h1>Dogesweeper</h1>
+        {/* <Timer /> */}
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', position: 'relative' }}>
+          {gameOver ? <Modal reset={reset} won={won} /> : <></>}
+          {grid.map((singleRow, i1) => {
+            return (
+              <div style={{ display: "flex" }} key={i1}>
+                {singleRow.map((cell, i2) => {
+                  return (
+                    <Cell
+                      details={cell}
+                      updateFlag={updateFlag}
+                      revealCell={revealCell}
+                      key={i2}
+                    />
+                  );
+                })}
+              </div>
+            );
+          })}
         </div>
-      </div>
-      {/* <Timer /> */}
-      <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column', position: 'relative'}}>
-        {gameOver ? <Modal reset={reset} won={won}/> : <></>}
-        {grid.map((singleRow, i1) => {
-          return (
-            <div style={{display: "flex"}} key={i1}>
-              {singleRow.map((cell, i2) => {
-                return (
-                  <Cell
-                    details={cell}
-                    updateFlag={updateFlag}
-                    revealCell={revealCell}
-                    key={i2}
-                  />
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
-      <div className="rules">
+        <div className="rules">
           <p>Clear the board without finding one of Max's presents to win. Right click on a square if you suspect there's a present there. If you find a present, it's game over.
           </p>
         </div>
-      <div className="difficulty">
-        <h2>Change Difficulty</h2>
-        <div className="difficulty-options">
-          <div>
-            <input
-              type="radio" id="beginner"
-              name="difficulty" value="beginner"
-              onClick={() => changeDifficulty(10, 10, 10)}
-            ></input>
-            <label htmlFor="beginner">Beginner(10 rows, 10 columns, 10 bombs)</label>
+        <div className="options">
+          <div className="difficulty">
+            <h2>Change Difficulty</h2>
+            <div className="difficulty-options">
+              <div>
+                <input
+                  type="radio" id="beginner"
+                  name="difficulty" value="beginner"
+                  onClick={() => changeDifficulty(10, 10, 10)}
+                ></input>
+                <label htmlFor="beginner">Beginner(10 rows, 10 columns, 10 bombs)</label>
+              </div>
+              <div>
+                <input
+                  type="radio" id="intermediate"
+                  name="difficulty" value="intermediate"
+                  onClick={() => changeDifficulty(16, 16, 40)}
+                ></input>
+                <label htmlFor="intermediate">Intermediate(16 rows, 16 columns, 40 bombs)</label>
+              </div>
+              <div>
+                <input
+                  type="radio" id="expert"
+                  name="difficulty" value="expert"
+                  onClick={() => changeDifficulty(16, 30, 99)}
+                ></input>
+                <label htmlFor="expert">Expert(16 rows, 30 columns, 99 bombs)</label>
+              </div>
+            </div>
+            <button onClick={() => reset()}>
+              Update Difficulty
+            </button>
           </div>
-          <div>
-          <input
-              type="radio" id="intermediate"
-              name="difficulty" value="intermediate"
-              onClick={() => changeDifficulty(16, 16, 40)}
-            ></input>
-            <label htmlFor="intermediate">Intermediate(16 rows, 16 columns, 40 bombs)</label>
-          </div>
-          <div>
-          <input
-              type="radio" id="expert"
-              name="difficulty" value="expert"
-              onClick={() => changeDifficulty(16, 30, 99)}
-            ></input>
-            <label htmlFor="expert">Expert(16 rows, 30 columns, 99 bombs)</label>
+          <div className="custom">
+            <h2>Custom Board</h2>
+            <div className="custom-options">
+              <div>
+                <label htmlFor="rows">Rows (10 - 100)</label>
+                <input
+                  type="number" id="rows"
+                  name="rows"
+                  min="10" max="100"
+                  onChange={(e) => setRows(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="cols">Columns (10 - 100)</label>
+                <input
+                  type="number" id="cols"
+                  name="cols"
+                  min="10" max="100"
+                  onChange={(e) => setCols(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="cols">Bombs (1 - 9,999)</label>
+                <input
+                  type="number" id="bombs"
+                  name="bombs"
+                  min="1" max="9999"
+                  onChange={(e) => setBombs(e.target.value)}
+                ></input>
+              </div>
+            </div>
+            <button onClick={() => reset()}>
+              Create Custom Board
+            </button>
           </div>
         </div>
-        <button
-            onClick={() => reset()}
-          >
-            Update Difficulty
-        </button>
       </div>
-    </div>
-    <div className="credits">Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      <div className="credits"><p>Created by Paula Herrera | Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p></div>
     </>
   );
 };
